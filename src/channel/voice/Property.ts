640dc1d79a0ec9af416b9357f7c3140279b4ac9c
Kyuri <107470858+Kyuri-jp@users.noreply.github.com>
2024-11-02 17:47:52 +0900

    catch

--- src/channel/voice/Property.ts
+++ src/channel/voice/Property.ts
@@ -2,7 +2,9 @@ import { VoiceChannel } from 'discord.js';
 import { client } from '../..';
 
 export async function GetUsers(channelID: string) {
-    const channel = await client.channels.fetch(channelID);
+    const channel = await client.channels.fetch(channelID).catch(error=>{
+        console.error(error)
+    });
 
     if (!(channel instanceof VoiceChannel)) {
         throw new Error("Channel is not a voice channel.");
